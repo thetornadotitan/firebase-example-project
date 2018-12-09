14c1ebecd3511474d63b1c7c9126b9af0af63f71
thetornadotitan <thetornadotitan@gmail.com>
2018-12-08 23:59:39 -0600

    second commit

--- info.php
+++ info.php
@@ -6,7 +6,7 @@
     $data["lang"] = "JS";
     $data["desc"] = "
         A learning project for myself to get in and start using and understanding <a class='custom-card-link' target='_blank' href='https://firebase.google.com/?authuser=0'>firebase</a>.<hr />
-        <a class='custom-card-link' target='_blank' href='https://github.com/thetornadotitan/p5js-Sandpiles'>Code</a> available on <a class='custom-card-link' target='_blank' href='https://github.com/thetornadotitan/'>my GitHub profile</a>.
+        <a class='custom-card-link' target='_blank' href='https://github.com/thetornadotitan/firebase-example-project'>Code</a> available on <a class='custom-card-link' target='_blank' href='https://github.com/thetornadotitan/'>my GitHub profile</a>.
     ";
 
     return $data;
